c055a1c8c352f9e49010278c992f4d42fd451269
Sibanand CIS <sibananda.k@cisinlabs.com>
2013-02-15 19:25:45 +0530

    extend form

--- app/models/spree/booking.rb
+++ app/models/spree/booking.rb
@@ -1,5 +1,5 @@
 class Spree::Booking < ActiveRecord::Base
-   attr_accessible :name,:pick_date,:del_date,:addr,:dest,:vol,:ph_no,:email,:park_zone,:length_rental
+   attr_accessible :name,:pick_date,:del_date,:addr,:dest,:vol,:ph_no,:email,:park_zone,:length_rental,:recurring
 
    validates :name,:pick_date,:del_date,:addr,:dest,:vol,:ph_no,:email,:length_rental , :presence => true
    validates_format_of :email, :with => /^.+@.+$/
