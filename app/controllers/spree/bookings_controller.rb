42c762ac411609784737a53e7f8ecfccf005f919
Sibanand CIS <sibananda.k@cisinlabs.com>
2013-02-22 13:10:16 +0530

    extend functionality

--- app/controllers/spree/bookings_controller.rb
+++ app/controllers/spree/bookings_controller.rb
@@ -9,23 +9,25 @@ class Spree::BookingsController < Spree::OrdersController
     @booking = Spree::Booking.find_by_id(params[:id])
   end
 
-   def create
+  def create
     @booking = Spree::Booking.new(params[:booking])
     if @booking.save
-      ActionMailer::Base::UserMailer.delay({ :run_at => 2.minutes.from_now}).welcome_email(@booking)   
-      @product = @booking.find_product
-      @qty = @booking.find_duration
-      populator = Spree::OrderPopulator.new(current_order(true), current_currency)    
-        add_to_cart_params = {:variants =>{@product.master.id.to_s=>@qty.to_i}}
-        if populator.populate(add_to_cart_params)
-          fire_event('spree.cart.add')
-          fire_event('spree.order.contents_changed')
-          respond_with(@order) do |format|
-            format.html { redirect_to cart_path }
-          end
-        end
-       else
-       render :new 
+        @array_of_products_and_qty = @booking.find_duration
+        @array_of_products_and_qty.each do |product|
+            populator = Spree::OrderPopulator.new(current_order(true), current_currency)    
+            add_to_cart_params = {:variants =>{product[:product].master.id.to_s=>product[:quantity].to_i}}
+            if populator.populate(add_to_cart_params)
+              fire_event('spree.cart.add')
+              fire_event('spree.order.contents_changed')              
+            end
+        end 
+        #render :new 
+       respond_with(@order) do |format|
+        format.html { redirect_to cart_path }
+       end      
+
+    else
+        render :new 
     end
   end
 
--- app/models/spree/booking.rb
+++ app/models/spree/booking.rb
@@ -4,18 +4,52 @@ class Spree::Booking < ActiveRecord::Base
    validates :name,:pick_date,:del_date,:addr,:dest,:vol,:ph_no,:email,:length_rental , :presence => true
    validates_format_of :email, :with => /^.+@.+$/
 
-  def find_product  
-    Spree::Product.find_by_name("1 Monat")
-  end
 
   def find_duration
-  	time_diff_components = Time.diff(Time.parse(self.del_date.to_s), Time.parse(self.pick_date.to_s),'%M')
-      day = time_diff_components[:day] 
-      month = time_diff_components[:month] 
-      if time_diff_components[:day] > 0
-        month = month+1
-      end 
-     month  
-  end 	
+    order_porducts = []
+  	time_diff_components = Time.diff(Time.parse(self.del_date.to_s), Time.parse(self.pick_date.to_s))
+    if time_diff_components[:month]  > 0
+        case time_diff_components[:month]
+          when 1..2 then 
+              order_porducts.push( {:product=> Spree::Product.find_by_name("1 Monat"),:quantity=>time_diff_components[:month]} )
+          when 3 then 
+              order_porducts.push( {:product=> Spree::Product.find_by_name("3 Monat"),:quantity=>1} )
+          when 4..5 then
+              order_porducts.push( {:product=> Spree::Product.find_by_name("3 Monat"),:quantity=>1} )
+              order_porducts.push( {:product=> Spree::Product.find_by_name("1 Monat"),:quantity=>(time_diff_components[:month]-3)} )
+          when 6 then 
+              order_porducts.push( {:product=> Spree::Product.find_by_name("6 Monat"),:quantity=>1} )
+          when 7..time_diff_components[:month] then 
+            order_porducts.push( {:product=> Spree::Product.find_by_name("6 Monat"),:quantity=>1} )
+            order_porducts.push( {:product=> Spree::Product.find_by_name("1 Monat"),:quantity=>(time_diff_components[:month]-6)} )
+        end
+      end  
+    if time_diff_components[:year] > 0   
+        case time_diff_components[:year]
+          when 1 then 
+              order_porducts.push( {:product=> Spree::Product.find_by_name("1 Year"),:quantity=>1} )
+          when 2 then 
+              order_porducts.push( {:product=> Spree::Product.find_by_name("2 Year"),:quantity=>1} )    
+          when 3..time_diff_components[:year] then     
+            order_porducts.push( {:product=> Spree::Product.find_by_name("2 Year"),:quantity=>1} )
+            order_porducts.push( {:product=> Spree::Product.find_by_name("1 Year"),:quantity=>time_diff_components[:year]-2} )
+        end 
+    end    
+    if time_diff_components[:week] > 0   
+        case time_diff_components[:week]
+          when 1 then 
+              order_porducts.push( {:product=> Spree::Product.find_by_name("1 week"),:quantity=>1} )
+        end     
+    end  
 
+    if time_diff_components[:day] > 0   
+        case time_diff_components[:day]
+          when 1 then 
+              order_porducts.push( {:product=> Spree::Product.find_by_name("1 day"),:quantity=>1} )
+          when 1..time_diff_components[:day] then 
+              order_porducts.push( {:product=> Spree::Product.find_by_name("1 day"),:quantity=>time_diff_components[:day]} )    
+        end     
+    end  
+    order_porducts
+  end
 end
